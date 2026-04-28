de5c596f70562520f46f527e8940557ed3e52e00
hontonoran <hontonoran@gmail.com>
2026-04-28 21:07:16 +0300

    add task-2 and task-3

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,23 @@
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = [...items];
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(name) {
+    this.#items.push(name);
+  }
+
+  removeItem(name) {
+    this.#items = this.#items.filter(item => item !== name);
+  }
+}
+
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
